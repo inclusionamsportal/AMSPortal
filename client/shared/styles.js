35de6704e225167daaad9914ca37384fdc1ac34e
treykris <trey10027@gmail.com>
2019-08-12 00:41:51 -0400

    shared styles

---
+++ client/shared/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const secondaryColor = '#53ccf1'
+export const secondaryColorHover = '#7ba5ca'
+export const whiteSmoke = '#ececec'
+export const white = '#fff'
+export const lightGray = '#dcdcdc'
+export const headingColor = '#333'
+export const red = '#c72525'
+export const brandyRose = '#bb7c75'
+
+export const SectionHeading = styled.h2`
+  color: ${headingColor};
+  border-bottom: 1px solid ${whiteSmoke};
+  margin-bottom: 0;
+  margin-top: 0.6rem;
+  padding-bottom: 0.2rem;
+`
