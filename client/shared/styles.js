44fde71bf79449031efc9858321db2b1af392973
treykris <trey10027@gmail.com>
2019-08-14 10:18:58 -0400

    updated styles

--- client/shared/styles.js
+++ client/shared/styles.js
@@ -5,14 +5,13 @@ export const secondaryColorHover = '#7ba5ca'
 export const whiteSmoke = '#ececec'
 export const white = '#fff'
 export const lightGray = '#dcdcdc'
-export const headingColor = '#333'
 export const red = '#c72525'
 export const brandyRose = '#bb7c75'
 export const green = '#44a23f'
 export const greenHover = '#77a274'
+export const black = '#333'
 
 export const SectionHeading = styled.h2`
-  color: ${headingColor};
   border-bottom: 1px solid ${whiteSmoke};
   margin-bottom: 0;
   margin-top: 0.6rem;
