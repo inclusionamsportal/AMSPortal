1c918c11b4a9dd8dd46043a208b03b34fe08d46f
treykris <trey10027@gmail.com>
2019-08-15 17:40:39 -0400

    find all comments associated with applicantName

--- server/api/comments.js
+++ server/api/comments.js
@@ -12,25 +12,38 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/:id', async (req, res, next) => {
+// router.get('/:id', async (req, res, next) => {
+//   console.log(`give me some weed yo`, req.params.id)
+//   try {
+//     const id = req.params.id
+//     const foundComment = await Comments.findByPk(id)
+//     res.json(foundComment)
+//   } catch (err) {
+//     res.status(err)
+//     next(err)
+//   }
+// })
+
+//find all the comments for a certain applicant/application
+router.get('/:applicantName', async (req, res, next) => {
   try {
-    const id = req.params.id
-    const foundComment = await Comments.findByPk(id)
-    res.json(foundComment)
+    const applicantName = req.params.applicantName
+    const comments = await Comments.findAll({
+      where: {applicantName},
+      order: [['id', 'DESC']]
+    })
+    res.json(comments)
   } catch (err) {
     res.status(err)
     next(err)
   }
 })
 
-//find all the comments for a certain applicant/application
-router.get('/:applicantName', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
-    const applicantName = req.params.applicantName
-    const appComment = await Comments.findAllByPk(applicantName)
-    res.json(appComment)
+    const newComment = await Comments.create(req.body)
+    res.status(201).send(newComment)
   } catch (err) {
-    res.status(err)
     next(err)
   }
 })
